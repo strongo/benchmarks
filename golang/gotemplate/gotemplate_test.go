1c0fd18fb869a76a3c675ffa269ae344b5d6b774
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2014-01-19 16:17:45 +0400

    Initial tests structure

--- golang/gotemplate/gotemplate_test.go
+++ golang/gotemplate/gotemplate_test.go
@@ -1,15 +1,33 @@
 package gotemplate
 
 import (
-	//"os"
+	"bytes"
 	"github.com/strongo/benchmarks"
 	"testing"
 	"text/template"
 )
 
-func BenchmarkGoTemplateSimple(b *testing.B) {
+func getSimpleTemplate() (*template.Template, error) {
+	return template.New("simple").Parse("Hello, {{.}}!")
+}
 
-	t, _ := template.New("simple").Parse("Hello, {{.}}!")
+func Test_Simple_GoTemplate(t *testing.T) {
+
+	t1, _ := getSimpleTemplate()
+
+	wr := new(bytes.Buffer)
+
+	t1.ExecuteTemplate(wr, "simple", "stranger")
+
+	if wr.String() != "Hello, stranger!" {
+		t.Errorf("Unexpected output")
+	}
+
+}
+
+func Benchmark_Simple_GoTemplate(b *testing.B) {
+
+	t, _ := getSimpleTemplate()
 
 	wr := new(benchmarks.DevNullWriter)
 
--- golang/strongo/strongo_test.go
+++ golang/strongo/strongo_test.go
@@ -2,25 +2,42 @@ package strongo
 
 import (
 	"io"
+	"bytes"
 	"testing"
+//	"github.com/strongo/templates"
 	"github.com/strongo/benchmarks"
 )
 
-func BenchmarkStrongo(b *testing.B) {
-	wr := new(benchmarks.DevNullWriter)
-	t := new(Template_simple)
-    for i := 0; i < b.N; i++ {
-		t.Render(wr)
-    }
+const USER = "stranger"
+
+func Test_Simple_Strongo(t *testing.T) {
+	template := new(Template_simple)
+	writer := new(bytes.Buffer)
+	template.Render(writer, USER)
+	s := writer.String()
+	if s != "Hello, stranger!" {
+		t.Errorf("Unexpected output")
+	}
+}
+
+func Benchmark_Simple_Strongo(b *testing.B) {
+	template := new(Template_simple)
+	writer := new(benchmarks.DevNullWriter)
+	for i := 0; i < b.N; i++ {
+		template.Render(writer, USER)
+	}
 }
 
 type Template_simple struct {
-
 }
 
-func (Template_simple) Render(wr io.Writer) (error){
-	wr.Write([]byte("Hello, "))
-	wr.Write([]byte("stranger"))
-	wr.Write([]byte("!"))
+//func (t *Template_simple) Render(writer io.Writer, context interface{}) (error){
+//	return t.Render2(writer, string(context))
+//}
+
+func (t *Template_simple) Render(writer io.Writer, user string) (error){
+	writer.Write([]byte("Hello, "))
+	writer.Write([]byte(user))
+	writer.Write([]byte("!"))
 	return nil
 }
